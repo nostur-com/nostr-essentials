ec4e2b11bd4aeb8d1e5c34d0e5b0b11dd86ee8b0
Fabian <fabian@nostur.com>
2023-11-23 12:32:37 +0100

    Fix secp256k1 version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMajor(from: "0.9.2")),
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMinor(from: "0.9.2")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMinor(from: "1.0.0")) // or `.upToNextMajor
 //        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.0.0")),
 //        .package(url: "https://github.com/jedisct1/swift-sodium", branch: "master")
